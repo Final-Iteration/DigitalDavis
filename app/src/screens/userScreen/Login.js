84c74378d7d1f6f5c06ab3b4f1046b6772b90b8e
gustavboyd <boydgustav@gmail.com>
2021-09-13 16:01:18 -0700

    added styling to email/password text input

--- app/src/screens/userScreen/Login.js
+++ app/src/screens/userScreen/Login.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet, Button, TextInput, Dimensions, Image } from "react-native";
+
+const {width, height} = Dimensions.get('window');
 
 const Login = (props) => {
+
   return (
     <View>
+      <StatusBar style="dark"/>
       {/* this will be removed! when we have our login API set up*/}
       <Text style={{ alignSelf: "center" }}>Log in screen</Text>
 
@@ -15,8 +20,27 @@ const Login = (props) => {
         title="Sign up"
         onPress={() => props.navigation.navigate("Register")}
       />
+      <TextInput style = {styles.emailPassStyle}
+        autoCapitalize = "none"
+        autoCorrect = {false}
+        placeholder = "Email"
+      />
+      <TextInput style = {styles.emailPassStyle}
+        autoCapitalize = "none"
+        autoCorrect = {false}
+        placeholder = "Password"
+      />
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  emailPassStyle: {
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: '#D3D3D3',
+    marginHorizontal: width / 10,
+    marginBottom: height / 40
+  }
+});
+
 export default Login;
