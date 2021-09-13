c70a30da9a2a2fa63c12323034546ff5f8d37186
Manhsy <63436025+Manhsy@users.noreply.github.com>
2021-09-13 16:15:49 -0700

    moved participate button

--- app/src/screens/challengesScreen/CurrentChallenge.js
+++ app/src/screens/challengesScreen/CurrentChallenge.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import ChallengeBox from "../../components/ChallengeBox";
+import ChallengeBox from "./components/ChallengeBox";
 
 const CurrentChallenges = (props) => {
   return (
     <View>
-      <Text style={{ alignSelf: "center" }}>
-        This is the Current Challenges screen
-      </Text>
-      <ChallengeBox/>
+      <ChallengeBox />
       <Button
         title="past challenge"
         onPress={() => {
