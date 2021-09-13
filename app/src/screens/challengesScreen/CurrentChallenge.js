5635d81e0e14a6c9eec0f7d637a6c0149f2691d4
2320sharon <sharonfitzpatrick@csus.edu>
2021-09-13 15:01:19 -0700

    Fixed: Dependency problem with excess package.json

--- app/src/screens/challengesScreen/CurrentChallenge.js
+++ app/src/screens/challengesScreen/CurrentChallenge.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import ChallengeBox from "../../components/ChallengeBox";
 
 const CurrentChallenges = (props) => {
   return (
     <View>
       <Text style={{ alignSelf: "center" }}>
-        This is the CurrentChallenges screen
+        This is the Current Challenges screen
       </Text>
+      <ChallengeBox/>
       <Button
         title="past challenge"
         onPress={() => {
