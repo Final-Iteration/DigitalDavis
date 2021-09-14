4d480d60d17cdcb6fec2eedfe7a05de58d77bf3a
gustavboyd <boydgustav@gmail.com>
2021-09-13 20:11:55 -0700

    created joinChallenges

---
+++ app/src/screens/challengesScreen/JoinChallenges.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const JoinChallenges = () => {
+    return (
+        <View>
+            <Text>Join Challenge Page</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create();
+
+export default JoinChallenges;
