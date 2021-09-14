44eeef9c1137eaf51e0eafc1d59e579859c4a394
gustavboyd <boydgustav@gmail.com>
2021-09-13 18:03:56 -0700

    added styling to change buttons text to whire

--- app/src/screens/userScreen/Login.js
+++ app/src/screens/userScreen/Login.js
@@ -12,21 +12,27 @@ const Login = (props) => {
         source = {imageSource}> 
         {/* this will be removed! when we have our login API set up*/}
         <View style = {styles.viewMargins}>
-          <Text style={styles.login}>Log in screen</Text>
+          <Text 
+            style={styles.login}>Log in screen
+          </Text>
           <Button
             title="Enter main flow"
+            color = "white"
             onPress={() => props.navigation.navigate("Main")}
           />
           <Button 
             title="Sign up"
+            color = "white"
             onPress={() => props.navigation.navigate("Register")}
           />
-          <TextInput style = {styles.emailPassStyle}
+          <TextInput 
+            style = {styles.emailPassStyle}
             autoCapitalize = "none"
             autoCorrect = {false}
             placeholder = "Email"
           />
-          <TextInput style = {styles.emailPassStyle}
+          <TextInput 
+            style = {styles.emailPassStyle}
             autoCapitalize = "none"
             autoCorrect = {false}
             placeholder = "Password"
@@ -37,14 +43,16 @@ const Login = (props) => {
 };
 const styles = StyleSheet.create({
   viewMargins: {
-    marginTop: height / 10
+    marginTop: height / 5
   },
   emailPassStyle: {
     height: 50,
     borderRadius: 5,
     backgroundColor: '#F6F6F6',
     marginHorizontal: width / 10,
-    marginBottom: height / 40
+    marginBottom: height / 40,
+    fontSize:18,
+    padding: height / 70
   },
   imageStyle:{
     height: height,
@@ -52,9 +60,6 @@ const styles = StyleSheet.create({
   },
   login:{
     alignSelf: 'center',
-    color: 'white'
-  },
-  buttonsStyle:{
     color: 'white'
   }
 });
