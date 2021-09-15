c6696b862b44614cce90b4fa811c043e1dc5c089
2320sharon <sharonfitzpatrick@csus.edu>
2021-09-14 23:41:13 -0700

    The gesture handler and animator work.

--- app/src/screens/challengesScreen/components/SwipeButton.js
+++ app/src/screens/challengesScreen/components/SwipeButton.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  runOnJS,
+  useAnimatedGestureHandler,
+} from "react-native-reanimated";
 
 const BUTTON_WIDTH = 350;
 const BUTTON_HEIGHT = 100;
@@ -11,7 +19,32 @@ const H_WAVE_RANGE = SWIPEABLE_DIMENSIONS + 2 * BUTTON_PADDING;
 const H_SWIPE_RANGE = BUTTON_WIDTH - 2 * BUTTON_PADDING - SWIPEABLE_DIMENSIONS;
 
 const SwipeButton = ({ onToggle }) => {
-  return <View style={styles.swipeCont}></View>;
+  const X = useSharedValue(0);
+
+  const AnimatedGestureHandler = useAnimatedGestureHandler({
+    onActive: (e) => {
+      X.value = e.translationX;
+      //updates the current X variable with the amount the user moved the button
+    },
+  });
+
+  const AnimatedStyles = {
+    swipeable: useAnimatedStyle(() => {
+      return {
+        transform: [{ translateX: X.value }],
+      };
+    }),
+  };
+
+  return (
+    <View style={styles.swipeCont}>
+      <PanGestureHandler onGestureEvent={AnimatedGestureHandler}>
+        <Animated.View
+          style={[styles.swipeable, AnimatedStyles.swipeable]}
+        ></Animated.View>
+      </PanGestureHandler>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -25,6 +58,23 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "row",
+    backgroundColor: "#DDDDDD",
+  },
+  swipeable: {
+    position: "absolute",
+    left: BUTTON_PADDING,
+    height: SWIPEABLE_DIMENSIONS,
+    width: SWIPEABLE_DIMENSIONS, //160
+    borderRadius: SWIPEABLE_DIMENSIONS,
+    zIndex: 3,
+    backgroundColor: "white",
+  },
+  swipeText: {
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    zIndex: 2,
+    color: "#1b9aaa",
   },
 });
 
