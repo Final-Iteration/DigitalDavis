2adf5bc5580c7345528fef8cfbf07745e4d30c52
gustavboyd <boydgustav@gmail.com>
2021-09-16 15:44:53 -0700

    Added email/pass validation/skip button to main

--- app/src/screens/userScreen/Login.js
+++ app/src/screens/userScreen/Login.js
@@ -1,44 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { StatusBar } from "expo-status-bar";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  TextInput,
-  Dimensions,
-  Image,
-  ImageBackground,
-  TouchableOpacity
-} from "react-native";
+import {View,Text,StyleSheet,Button,TextInput,Dimensions,Image,ImageBackground,TouchableOpacity} from "react-native";
 
 const { width, height } = Dimensions.get("window");
 const imageSource = require("../../../assets/blurredDavis.jpg");
 
 const Login = (props) => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const blankInputCheck = () => {
+    if (email.length === 0 || password.length === 0){
+      return;
+    } else if (validateEmail(email)){
+      props.navigation.navigate("Main")
+    }
+  };
+
+  function validateEmail (email) {
+    const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexp.test(email);
+  }
+
   return (
       <ImageBackground style = {styles.imageStyle}
         source = {imageSource}> 
         {/* this will be removed! when we have our login API set up*/}
         <View style = {styles.viewMargins}>
-          <Text 
-            style={styles.login}>Log in screen
+          <Text style={styles.login}>
+            Log In
           </Text>
-          <Button
-            title="Enter main flow"
-            color = "white"
-            onPress={() => props.navigation.navigate("Main")}
-          />
-          <Button 
-            title="Sign up"
-            color = "white"
-            onPress={() => props.navigation.navigate("Register")}
-          />
           <TextInput 
             style = {styles.emailPassStyle}
             autoCapitalize = "none"
             autoCorrect = {false}
             placeholder = "Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
           />
           <TextInput 
             style = {styles.emailPassStyle}
@@ -46,12 +45,30 @@ const Login = (props) => {
             autoCorrect = {false}
             secureTextEntry={true}
             placeholder = "Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
           />
           <View style={styles.signInView}>
-            <TouchableOpacity onPress={() => props.navigation.navigate("Main")}>
-              <Text style={styles.signUpButton}>Sign In</Text>
+            <TouchableOpacity onPress={() => blankInputCheck()}>
+              <Text style={[(email != 0 && password != 0) ? styles.signUpButtonGood : styles.signUpButtonBad]}>Sign In</Text>
             </TouchableOpacity>
           </View>
+          <View style = {styles.bottomHeader}>
+            <Button
+              title="Forgot Password?"
+              color = "white"
+            />
+            <Button
+              title="Sign up"
+              color = "white"
+              onPress={() => props.navigation.navigate("Register")}
+            />
+          </View>
+          <Button
+              title="SKIP BUTTON TO MAIN APP"
+              color = "white"
+              onPress={() => props.navigation.navigate("Main")}
+          />
         </View>
       </ImageBackground>
   );
@@ -76,9 +93,18 @@ const styles = StyleSheet.create({
   login: {
     alignSelf: "center",
     color: "white",
+    fontWeight: "bold",
+    fontSize: 35,
+    paddingBottom: height / 50,
   },
-  signUpButton: {
+  signUpButtonGood: {
     color: "white",
+    alignSelf: "center",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  signUpButtonBad:{
+    color: "grey",
     alignSelf: "center",
     fontSize: 20,
     marginVertical: 10,
@@ -89,6 +115,10 @@ const styles = StyleSheet.create({
     width: width / 1.24,
     backgroundColor: "#142A4F",
     borderRadius: 10,
+  },
+  bottomHeader:{
+    flexDirection: 'row',
+    alignSelf: 'center'
   },
 });
 
