d1f0fdc651b3831bd0f6173e7dc3b3fc088b1fc1
2320sharon <sharonfitzpatrick@csus.edu>
2021-09-15 19:52:41 -0700

    Challenges : swipe button for active & recent done

--- app/src/screens/challengesScreen/CurrentChallenge.js
+++ app/src/screens/challengesScreen/CurrentChallenge.js
@@ -1,22 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
 import ChallengeBox from "./components/ChallengeBox";
 import SwipeButton from "./components/SwipeButton";
-import SwipeButton2 from "./components/SwipeButton2";
 
 const CurrentChallenges = (props) => {
+  //Toggles the active and recent challenges
   const [toggleState, setToggleState] = useState(false);
 
   const handleToggle = (value) => setToggleState(value);
+
+  useEffect(() => {
+    if (toggleState) {
+      // console.log("Toggle is true. Go to on Recent Challenges Screen");
+      props.navigation.navigate("SearchedChallenge");
+    } else {
+      // console.log("Toggle is FALSE.Stay Active Challenges Screen");
+    }
+  }, [toggleState]);
+
   return (
     <View>
-      <Button
+      {/* This Button is no longer needed as its done by the swipe button */}
+      {/* <Button
         title="past challenge"
         onPress={() => {
           props.navigation.navigate("SearchedChallenge");
         }}
-      />
-      <SwipeButton2 onToggle={handleToggle} />
+      /> */}
+      <SwipeButton onToggle={handleToggle} />
       <ScrollView>
         <ChallengeBox />
         <ChallengeBox />
--- app/src/screens/challengesScreen/components/SwipeButton.js
+++ app/src/screens/challengesScreen/components/SwipeButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState,useRef } from "react";
+import { useState, useRef } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import Animated, {
@@ -13,50 +13,111 @@ import Animated, {
 } from "react-native-reanimated";
 
 const BUTTON_WIDTH = 350;
-const BUTTON_HEIGHT = 100;
+const BUTTON_HEIGHT = 80;
 const BUTTON_PADDING = 10;
 const SWIPEABLE_DIMENSIONS = BUTTON_HEIGHT - 2 * BUTTON_PADDING;
+const SWIPEABLE_DIMENSIONS_WIDTH = BUTTON_HEIGHT - 2 * BUTTON_PADDING + 70;
 
 const H_WAVE_RANGE = SWIPEABLE_DIMENSIONS + 2 * BUTTON_PADDING;
-const H_SWIPE_RANGE = BUTTON_WIDTH - 2 * BUTTON_PADDING - SWIPEABLE_DIMENSIONS;
+const H_SWIPE_RANGE =
+  BUTTON_WIDTH - 2 * BUTTON_PADDING - SWIPEABLE_DIMENSIONS_WIDTH;
 
 const SwipeButton = ({ onToggle }) => {
-  const X = useSharedValue(0);
-    // Toggled State
-  const [toggled, setToggled] = useState(false);
-  const buttonText=useRef("Active");
+  const ActiveBold = useSharedValue("bold");
+  const ActiveColor = useSharedValue("black");
+  const ActiveZIndex = useSharedValue(4);
 
-  // //thie useEffect function is called whenever the state of toggled is modified
-  // useEffect(() => {
-  //   if(toggled){
-  //     buttonText.current = 'Recent'
-  //   }
-  //   else{
-  //     buttonText.current = 'Active'
-  //   }
-  // }, [toggled])
+  const RecentBold = useSharedValue("normal");
+  const RecentColor = useSharedValue("grey");
+  const RecentZIndex = useSharedValue(2);
+
+  const X = useSharedValue(0);
+  // Toggled State
+  const [toggled, setToggled] = useState(false);
+
+  // Fires when animation ends
+  const handleComplete = (isToggled) => {
+    if (isToggled !== toggled) {
+      setToggled(isToggled);
+      onToggle(isToggled);
+    }
+  };
 
   const AnimatedGestureHandler = useAnimatedGestureHandler({
-    onActive: (e) => {
+    onStart: (_, context) => {
+      //when a gesture is started start keeping track of the state of toggled
+      context.completed = toggled;
+    },
+
+    onActive: (e, context) => {
       X.value = e.translationX;
       //updates the current X variable with the amount the user moved the button
+      let newValue;
+
+      if (context.completed) {
+        //if the it is in the toggle state:
+        //update newValue to the amount you can swipe and the amount that was moved
+        newValue = H_SWIPE_RANGE + e.translationX;
+      } else {
+        //if the button is not in the toggled state
+        //newValue is just the amount the button was moved
+        newValue = e.translationX;
+      }
+      //If the newValue is in the Horizontal swipe range then move X (the button) to that place
+      if (newValue >= 0 && newValue <= H_SWIPE_RANGE) {
+        X.value = newValue;
+      }
     },
 
     onEnd: (e) => {
       if (X.value < BUTTON_WIDTH / 2 - SWIPEABLE_DIMENSIONS / 2) {
         X.value = withSpring(0); // if  X (the button) is moved < halfway across the screen snap back to origin
+        RecentBold.value = "normal";
+        RecentColor.value = "grey";
+        ActiveZIndex.value = 4;
+
+        ActiveColor.value = "black";
+        ActiveBold.value = "bold";
+        RecentZIndex.value = 2;
+        runOnJS(handleComplete)(false);
       } else {
         X.value = withSpring(H_SWIPE_RANGE);
+        RecentBold.value = "bold";
+        RecentColor.value = "black";
+        RecentZIndex.value = 4;
+
+        ActiveZIndex.value = 2;
+        ActiveBold.value = "normal";
+        ActiveColor.value = "grey";
+        runOnJS(handleComplete)(true);
       }
     },
   });
 
   const InterpolateXInput = [0, H_SWIPE_RANGE];
   //The value to transition x from starting position to Horizontal Swipe Range
+
   const AnimatedStyles = {
+    swipeCont: useAnimatedStyle(() => {
+      return {};
+    }),
     swipeable: useAnimatedStyle(() => {
       return {
         transform: [{ translateX: X.value }],
+      };
+    }),
+    ActiveText: useAnimatedStyle(() => {
+      return {
+        fontWeight: ActiveBold.value,
+        color: ActiveColor.value,
+        zIndex: ActiveZIndex.value,
+      };
+    }),
+    RecentText: useAnimatedStyle(() => {
+      return {
+        fontWeight: RecentBold.value,
+        color: RecentColor.value,
+        zIndex: RecentZIndex.value,
       };
     }),
     swipeText: useAnimatedStyle(() => {
@@ -82,19 +143,19 @@ const SwipeButton = ({ onToggle }) => {
   };
 
   return (
-    <View style={styles.swipeCont}>
-      <Text style={[styles.swipeText, AnimatedStyles.swipeText]}>
-        Current
-      </Text>
+    <Animated.View style={[styles.swipeCont, AnimatedStyles.swipeCont]}>
+      <Animated.Text style={[styles.ActiveText, AnimatedStyles.ActiveText]}>
+        Active
+      </Animated.Text>
       <PanGestureHandler onGestureEvent={AnimatedGestureHandler}>
         <Animated.View
           style={[styles.swipeable, AnimatedStyles.swipeable]}
         ></Animated.View>
       </PanGestureHandler>
-      <Animated.Text style={[styles.swipeText, AnimatedStyles.swipeText]}>
+      <Animated.Text style={[styles.RecentText, AnimatedStyles.RecentText]}>
         Recent
       </Animated.Text>
-    </View>
+    </Animated.View>
   );
 };
 
@@ -115,17 +176,22 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: BUTTON_PADDING,
     height: SWIPEABLE_DIMENSIONS,
-    width: SWIPEABLE_DIMENSIONS, //160
+    width: SWIPEABLE_DIMENSIONS_WIDTH, //160
     borderRadius: SWIPEABLE_DIMENSIONS,
     zIndex: 3,
     backgroundColor: "white",
   },
-  swipeText: {
+  ActiveText: {
     alignSelf: "center",
+    alignItems: "flex-start",
+    right: 70,
     fontSize: 20,
-    fontWeight: "bold",
-    zIndex: 2,
-    color: "#1b9aaa",
+  },
+  RecentText: {
+    alignSelf: "center",
+    alignItems: "flex-start",
+    left: 70,
+    fontSize: 20,
   },
 });
 
