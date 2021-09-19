dc95ffdafcc59baf129315c2fad7dc60efa4351f
gustavboyd <boydgustav@gmail.com>
2021-09-19 00:33:24 -0700

    changed the joinchallenges file to challengeinfo

---
+++ app/src/screens/challengesScreen/ChallengeInfo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const ChallengeInfo = () => {
+    return (
+        <View>
+            <Text>Challenge Information Page</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create();
+
+export default ChallengeInfo;
