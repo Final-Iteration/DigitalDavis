1b3344273d7993be85368ae0d2ba57c2c6db3481
gustavboyd <boydgustav@gmail.com>
2021-09-19 21:22:42 -0700

    created the createchallenge file with boilerPlate

---
+++ app/src/screens/challengesScreen/CreateChallengeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const CreateChallengeScreen = () => {
+    return (
+        <View>
+            <Text>
+                Create Challenge Screen
+            </Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({});
+
+export default CreateChallengeScreen;
