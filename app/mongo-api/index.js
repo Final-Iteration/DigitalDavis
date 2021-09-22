8c8de19ac931eb3091cdf6032ce51aaed8b0c473
Joshua Poe <42955626+icarus44-zer0@users.noreply.github.com>
2021-09-22 10:44:22 -0700

    added support for HELMET and Morgan

--- app/mongo-api/index.js
+++ app/mongo-api/index.js
@@ -1,43 +1,77 @@
+
 const dotenv = require('dotenv').config();
 const Joi = require('joi'); 
-const express = require('express')
-
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan')
 const app = express();
+// const logger = require('./logger');
+// const authenticator = require('./authenticator');
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
+
+app.use(helmet());
+
+
+const env = process.env.NODE_ENV || 'development'
+console.log(`Starting server in ${env} mode`);
+if (env === 'development'){
+    console.log(`Morgan enabled`);
+    app.use(morgan('dev'));
+}; 
+
+
+/**
+ * Custom Middleware
+ */
+// app.use(logger);
+// app.use(authenticator);
 
 
 const challenges = [
-    {id: 1, name: 'challenge demo 1'},
-    {id: 2, name: 'challenge demo 2'},
-    {id: 3, name: 'challenge demo 3'},
+    {id: 1, name: 'challenge sample 1'},
+    {id: 2, name: 'challenge sample 2'},
+    {id: 3, name: 'challenge sample 3'},
 ];
 
+
+/**
+ * 
+ */
 app.get('/api', (req,res) => {
     res.send('Hello World')
 });
 
+
+/**
+ * 
+ */
 app.get('/api/challenges', (req,res) => {
     res.send(challenges);
 });
 
-app.get('/api/challenge/:id', (req,res) => {
+
+/**
+ * 
+ */
+app.get('/api/challenges/:id', (req,res) => {
     const challenge = challenges.find(c => c.id === parseInt(req.params.id));
-    if (!challenge) res.status(404).send('ERROR 404 Given Challenge ID was not found');
+    if (!challenge) return res.status(404).send('Challenge ID not found');
     res.send(challenge);
 });
 
 /**
- * @todo move to asyn post methods for schema validation 
+ * @todo move to asyn methods for schema validation 
+ * 
+ * @
  */ 
 app.post('/api/challenges', (req,res) => {
     const {error} = validate_challenge(req.body)
 
-    if (error){
-        res.status(400).send(error.details[0].message)
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message)
 
     const challenge = {
         id: challenges.length + 1,
@@ -48,36 +82,65 @@ app.post('/api/challenges', (req,res) => {
     res.send(challenge);
 });
 
+/**
+ * 
+ */
 app.put('/api/challenges/:id', (req,res) => {
     const challenge = challenges.find(c => c.id === parseInt(req.params.id));
-    if (!challenge) res.status(404).send('ERROR 404 Given challenge ID was not found');
-
+    if (!challenge) return res.status(404).send('Challenge ID not found');
+    
     const {error} = validate_challenge(req.body)
 
-    if (error){
-        res.status(400).send(error.details[0].message)
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message);
+
     
     challenge.name = req.body.name;
     res.send(challenge);
 });
 
+/**
+ * 
+ */
+app.delete('/api/challenges/:id', (req,res) => {
+    const challenge = challenges.find(c => c.id === parseInt(req.params.id));
+    if (!challenge) return res.status(404).send('Challenge ID not found');
+
+    const index = challenges.indexOf(challenge)
+    challenges.splice(index,1)
+    res.send(challenge)
+});
+
+/**
+ * 
+ */
 function validate_challenge(challenge) {
     const challenges_schema = Joi.object({ 
         name: Joi.string()
             .min(3)
             .max(30)
-            .pattern(new RegExp('^[a-zA-Z0-9_ ]*$'))
+            .pattern(new RegExp('^[a-zA-Z0-9_ ]$'))
             .required(),
     })
 
-    const result = challenges_schema.validate({ 
+    return result = challenges_schema.validate({ 
         name: challenge.name
     });
-    
-    return result;
+
 };
+
+
+/**
+ * 
+ */
+function locate_challenge(id) {
+    const result = challenges.find(c => c.id === parseInt(id));
+    if (!result) 
+        return undefined;
+    else 
+        return result;
+
+};
+
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port ${port}`));
