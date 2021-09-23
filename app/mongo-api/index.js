2b221347ac07652e7501a208721fbaae42b60bc1
Joshua Poe <42955626+icarus44-zer0@users.noreply.github.com>
2021-09-23 16:49:24 -0700

    Added Schema and API calls to MongoDB docker cont.

--- app/mongo-api/index.js
+++ app/mongo-api/index.js
@@ -1,51 +1,136 @@
 // Config
-const dotenv = require('dotenv').config();
-const config = require('config');
+const dotenv = require("dotenv").config();
+const config = require("config");
 
 // Routes
-const challenges = require('./routes/challenges.js')
-const users = require('./routes/users.js')
+const challenges = require("./routes/challenges.js");
+const users = require("./routes/users.js");
 
-// Debugger 
-const startupDebugger = require('debug')('app:startup');
-const mongodbDebugger = require('debug')('app:mongodb');
-
+// Debugger
+const startupDebugger = require("debug")("app:startup");
+const mongodbDebugger = require("debug")("app:mongodb");
 
 //Middleware
-
-const helmet = require('helmet');
-const morgan = require('morgan')
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 // Express
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Start
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(helmet());
 
 //Router
-app.use('/api/challenges',challenges)
-app.use('/api/users',users)
+app.use("/api/challenges", challenges);
+app.use("/api/users", users);
 
-// NODE_ENV
-const env = process.env.NODE_ENV || 'development'
+// Debugger
+const env = process.env.NODE_ENV || "development";
 
-if (env === 'development'){
+if (env === "development") {
+  startupDebugger("Application Name: " + config.get("name"));
+  startupDebugger(`Morgan: enabled`);
 
-    startupDebugger('Application Name: ' + config.get('name'))
-    startupDebugger(`Morgan: enabled`);
+  mongodbDebugger(
+    "Database.Host: " +
+      config.get("mongodb-local-dev.protocol") +
+      config.get("mongodb-local-dev.host")
+  );
+  mongodbDebugger("Database.Name: " + config.get("mongodb-local-dev.database"));
+  mongodbDebugger(
+    "Database.Collection: " + config.get("mongodb-local-dev.collection")
+  );
+  app.use(morgan("dev"));
+}
 
-    mongodbDebugger('Database.Host: ' + config.get('mongodb.host'))
-    mongodbDebugger('Database.Name: ' + config.get('mongodb.database'))
-    mongodbDebugger('Database.Collection: ' + config.get('mongodb.collection'))
+// Mongoose
+const mongoose = require("mongoose");
+const { object } = require("joi");
+mongoose
+  .connect(
+    config.get("mongodb-local-dev.protocol") +
+      config.get("mongodb-local-dev.host") +
+      ":" +
+      config.get("mongodb-local-dev.port") +
+      "/" +
+      config.get("mongodb-local-dev.database")
+  )
+  .then(() => mongodbDebugger("Status: connected"));
 
-    app.use(morgan('dev'));
-}; 
+const challengeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: false,
+  },
+  summary: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  timestamps: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  start_date: {
+    type: Date,
+    required: false,
+  },
+  end_date: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  }
+});
+
+
+async function createChallenge(){
+    try {
+        const Challenge = mongoose.model("Challenge", challengeSchema);
+        const challenge = new Challenge({
+        name: "Test Challenge",
+        id: await makeid(16),
+        creator: "Josh Poe",  
+        });
+        const result = await challenge.save();
+        mongodbDebugger(result + 'added to database');
+    } catch (err) {
+        mongodbDebugger('err' + err);
+    }
+}
+
+async function makeid(length) {
+  var result = "";
+  var characters =
+    "0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+// createChallenge();
 
 // Server
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`listening on port ${port}`));
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => startupDebugger(`listening on port ${port}`));
