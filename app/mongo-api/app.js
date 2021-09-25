cd946bf35a1c05f3e92faa4f54e42427cf60dcb7
Joshua Poe <42955626+icarus44-zer0@users.noreply.github.com>
2021-09-25 13:02:00 -0700

    moved express module to seperate file

---
+++ app/mongo-api/app.js
@@ -0,0 +1,62 @@
+const dotenv = require("dotenv").config();
+const config = require("config");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const express = require("express");
+// const routes = require('./routes');
+const appDebugger = require("debug")("app:startup");
+
+
+const app = express();
+
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+app.use(mongoSanitize());
+
+
+// gzip compression
+// app.use(compression());
+
+// enable cors
+// app.use(cors());
+// app.options('*', cors());
+
+// jwt authentication
+// app.use(passport.initialize());
+// passport.use('jwt', jwtStrategy);
+
+// limit repeated failed requests to auth endpoints
+// if (config.env === 'production') {
+//   app.use('/v1/auth', authLimiter);
+// }
+
+// api routes
+// app.use(routes);
+
+try {
+    const port = process.env.EXPRESS_API_PORT || 3000;
+    if (process.env.NODE_ENV === "development") {
+        appDebugger("Application:" + config.get("development.name"));
+        app.use(morgan("dev"));
+        app.listen(port, () => appDebugger(`API listening on port ${port}`));
+      }
+     else {
+         app.listen(port);
+     } 
+} catch (error) {
+    appDebugger(error.message)
+}
+
+module.exports = app;
