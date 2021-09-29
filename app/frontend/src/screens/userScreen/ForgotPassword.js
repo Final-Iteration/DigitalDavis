8484b059fc669e7ee464d8d6a53fa4be2d65ce82
gustavboyd <boydgustav@gmail.com>
2021-09-29 15:56:33 -0700

    created forgot password screen

---
+++ app/frontend/src/screens/userScreen/ForgotPassword.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const ForgotPassword = () => {
+    return (
+        <View>
+            <Text>
+                Forgot Password Screen
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default ForgotPassword;
