5eb35de520dff687f564ca5243550ec71eacffd9
gustavboyd <boydgustav@gmail.com>
2021-09-30 02:03:28 -0700

    styled the forgot password scren

--- app/frontend/src/screens/userScreen/ForgotPassword.js
+++ app/frontend/src/screens/userScreen/ForgotPassword.js
@@ -1,16 +1,112 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import React,{useState} from "react";
+import {View, Text, StyleSheet, Dimensions, TextInput, TouchableOpacity, Alert} from 'react-native';
 
-const ForgotPassword = () => {
+const { width, height } = Dimensions.get('window');
+
+const ForgotPassword = (props) => {
+    const [email, setEmail] = useState('');
+
+    const blankEmailCheck = () => {
+        if (email.length === 0) {
+            return null;
+        } else if (validateEmail(email)) {
+            return false;
+        }
+      };
+
+    // a function that validates an email to the format of name@domain.com
+    function validateEmail(email) {
+        const regexp =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regexp.test(email);
+    }
+
+    const resetPasswordAlert = () => {
+        Alert.alert(
+            "Reset Password",
+            "An email has been sent to {email goes here}. Click the link included in that message to reset your password.",
+            [
+                {
+                    text: 'OK',
+                    onPress:() => props.navigation.navigate('Login'),
+                    // blankEmailCheck()
+                }
+            ]
+        );
+    };
+
     return (
-        <View>
-            <Text>
-                Forgot Password Screen
+        <View style = {styles.topTextContainer}>
+            <Text style = {styles.topText}>
+                Please enter your email address, and we'll send you
             </Text>
+            <Text style = {styles.topText}>
+                info on how to reset your password.
+            </Text>
+            <TextInput
+                style={styles.emailBox}
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Email Address"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TouchableOpacity style = {styles.resetPasswordButton} onPress={resetPasswordAlert}>
+                <Text style = {styles.resetPasswordText}>
+                    Reset Password
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    topTextContainer:{
+        marginTop: height / 60,
+    },
+    topText:{
+        alignSelf: 'center',
+        fontWeight: '300',
+    },
+    emailBox:{
+        height: height / 16,
+        borderRadius: 5,
+        backgroundColor: '#F6F6F6', 
+        marginHorizontal: width / 20,
+        fontSize: 17,
+        padding: height / 50,
+        top: height / 50,
+        shadowColor: '#470000',
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 0.2,
+        elevation: 1,
+    },
+    invalidEmailBox:{
+        height: height / 16,
+        borderRadius: 5,
+        backgroundColor: '#F6F6F6', 
+        marginHorizontal: width / 20,
+        fontSize: 17,
+        padding: height / 50,
+        top: height / 50,
+        shadowColor: 'red',
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 1,
+        elevation: 1,
+    },
+    resetPasswordButton:{
+        height: height / 16,
+        borderRadius: 5,
+        backgroundColor: '#E4E4E4', 
+        marginHorizontal: width / 20,
+        padding: height / 50,
+        top: height / 30,
+    },
+    resetPasswordText:{
+        fontWeight: '300',
+        alignSelf: 'center',
+        fontSize: 17,
+    }
+});
 
 export default ForgotPassword;
