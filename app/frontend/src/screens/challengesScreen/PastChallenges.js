92d2d2b4e720713da10aacf206aed54b149983db
gustavboyd <boydgustav@gmail.com>
2021-10-04 03:17:48 -0700

    setup past challenges page

--- app/frontend/src/screens/challengesScreen/PastChallenges.js
+++ app/frontend/src/screens/challengesScreen/PastChallenges.js
@@ -1,16 +1,91 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import ChallengeBox from './components/ChallengeBox';
+// import SwipeButton from "./components/SwipeButton";
 
-const PastChallenges = () => {
+const { width, height } = Dimensions.get('window');
+
+const PastChallenges = (props) => {
+  const [active, setActive] = useState(true)
+  const [recent, setRecent] = useState(true)
+
   return (
-    <View>
-      <Text style={{ alignSelf: 'center' }}>
-        This is the PastChallenges screen
-      </Text>
+    <View style={styles.viewContainer}>
+
+      <View style = {styles.backgroundPillContainer}>
+        <TouchableOpacity style = {recent ? styles.activePill : null} onPress = {() => {setRecent(true); setActive(true)}}>
+          <Text style = {active ? styles.inUseText : styles.notInUseText}>
+            Active
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style = {recent ? null : styles.pastPill} onPress = {() => {setRecent(false); setActive(false)}}>
+          <Text style = {active ? styles.notInUseText : styles.inUseText} >
+            Past
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* <View style={styles.longLine} /> */}
+      <ScrollView>
+        <TouchableOpacity onPress={() => props.navigation.navigate('ChallengeInformation')}>
+          <ChallengeBox current = {false}/>
+        </TouchableOpacity>
+        <ChallengeBox current = {false}/>
+        <ChallengeBox current = {false}/>
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewContainer: {
+    paddingBottom: height / 6,
+  },
+  backgroundPillContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: height / 70,
+    height: height / 16,
+    borderRadius: 30,
+    backgroundColor: '#f2f2f2', 
+    marginHorizontal: width / 5.5,
+    shadowColor: '#470000',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.5,
+    elevation: 1,
+  },
+  notInUseText:{
+    fontSize: 24,
+    fontWeight: '200',
+    marginVertical: 11,
+  },
+  inUseText:{
+    fontSize: 24,
+    color: '#142A4F',
+    marginVertical: 11,
+  },
+  activePill: {
+    height: height / 16,
+    width: width / 3.1,
+    borderRadius: 30,
+    backgroundColor: 'white',
+    right: width / 16,
+    alignItems: 'center'
+  },
+  pastPill: {
+    height: height / 16,
+    width: width / 3.1566,
+    borderRadius: 30,
+    backgroundColor: 'white',
+    left: width / 20,
+    alignItems: 'center'
+  },
+});
 
 export default PastChallenges;
