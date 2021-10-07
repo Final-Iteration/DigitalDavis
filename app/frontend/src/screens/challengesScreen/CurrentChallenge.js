19daec2ca892a112886717a132805c528d4f055d
Manhsy <63436025+Manhsy@users.noreply.github.com>
2021-10-07 01:33:22 -0700

    warning removed

--- app/frontend/src/screens/challengesScreen/CurrentChallenge.js
+++ app/frontend/src/screens/challengesScreen/CurrentChallenge.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet } from "react-native";
 import TopSwipe from "./components/TopSwipe";
 
 const CurrentChallenges = (props) => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <TopSwipe props={props} />
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <TopSwipe props={props} />;
 };
 
 const styles = StyleSheet.create({});
