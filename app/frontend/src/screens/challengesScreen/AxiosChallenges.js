6450b8b7e298bc55c04f2bdd49017d9c8793995d
Dennis Worley Jr <dennisw95.dw@gmail.com>
2021-10-16 19:49:17 -0700

    [#530] Created very basic functions in Axios to get challanges with or without ID, and to post a challenge. Need to figure out how to start testing the functionality with the UI by replacing the mockdata with whats being returned here.

--- app/frontend/src/screens/challengesScreen/AxiosChallenges.js
+++ app/frontend/src/screens/challengesScreen/AxiosChallenges.js
@@ -7,19 +7,59 @@ import axios from 'axios';
 const baseURL = 'http://localhost:3005/api/challenges/';
 
 export default class ChallengeList {
-  state = {
-    challenges: [],
-  };
-
-  getChallengesList() {
+  getChallengesListNoId = () => {
     //Will need to turn this into a constructor where our components can use
     // use this later on to allow it run everytime the component mounts
-    const getChallenges = () => {
-      axios.get(baseUrl).then((res) => {
+    axios
+      .get(baseURL)
+      .then((res) => {
         //We would recieve a array of all the challenges data here.
         res.data;
-        // this.setState({ courses: res.data });;
-      });
-    };
-  }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  getChallengesListWithId = (_id) => {
+    axios
+      .get(baseURL + _id)
+      .then((res) => {
+        //We would recieve the array of data for 1 id
+        res.data;
+      })
+      .catch((error) => console.log(error));
+  };
+
+  //could call this function and pass in the data as parameters
+  postChallenge = (
+    _name,
+    _creator,
+    _tags,
+    _description,
+    _location,
+    _timestamp,
+    _start_date,
+    _end_date,
+    _participants
+  ) => {
+    //Will need to turn this into a constructor where our components can use
+    // use this later on to allow it run everytime the component mounts
+    axios
+      .post(baseURL, {
+        data: {
+          challenge: {
+            name: _name,
+            creator: _creator,
+            tags: [_tags],
+            description: _description,
+            location: _location,
+            timestamp: _timestamp,
+            start_date: _start_date,
+            end_date: _end_date,
+            participants: _participants,
+          },
+        },
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.error(err));
+  };
 }
