1638c9bee95f853459a56abc4b46e37f51347307
Joshua <jpoe@csus.edu>
2021-10-16 22:31:38 -0700

    [#629] set email validation to joi implementation

--- app/mongo-api/validate/users.validation.js
+++ app/mongo-api/validate/users.validation.js
@@ -5,8 +5,7 @@ const createUser = {
   body: Joi.object().keys({
     first_name: Joi.string().min(1).max(150).trim().required(),
     last_name: Joi.string().min(1).max(150).trim().required(),
-    // email: Joi.string().required().min(3).max(30).trim(), //Add function to validate email
-    email: Joi.string().required().custom(email),
+    email: Joi.string().required().email(),
     dob: Joi.date().raw(),
     job_title: Joi.array().items(Joi.string()),
     department: Joi.string().trim(),
