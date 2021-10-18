d31b95881ecd5345e54da15cd2da73762b8194c5
2320sharon <sharonfitzpatrick@csus.edu>
2021-10-18 11:27:47 -0700

    ChalllengeFixture creates correct challenge format

--- app/mongo-api/tests/fixtures/challenge.fixture.js
+++ app/mongo-api/tests/fixtures/challenge.fixture.js
@@ -2,31 +2,96 @@ const mongoose = require("mongoose");
 const faker = require("faker");
 const { Challenge } = require("../../models");
 
+const challengeTags = [
+  "Emotional",
+  "Environmental",
+  "Intellectual",
+  "Physical",
+  "Social",
+  "Spiritual",
+];
+
+/**
+ *
+ *@todo fix copied shuffle code [DUPLICATED CODE]
+ */
+/**
+ * Returns the first element in the challengeTags array after the array has been shuffled
+ * @param {*} array
+ * @returns
+ */
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array[0];
+}
+
 const challengeOne = {
   _id: mongoose.Types.ObjectId(),
-  first_name: "A Running Challenge",
-  last_name: faker.name.findName(),
-  email: faker.internet.email().toLowerCase(),
-  job_title: [`${faker.name.jobTitle()}`],
-  department: faker.commerce.department(),
+  name: "A Running Challenge",
+  creator: faker.lorem.words(3).substring(0, 30),
+  tags: shuffle(challengeTags),
+  description: faker.random.words(),
+  location: faker.address.city(),
+  timestamp: faker.date.recent(),
+  start_date: faker.date.soon(),
+  end_date: faker.date.future(),
+  participants: [
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+  ],
 };
 
 const challengeTwo = {
   _id: mongoose.Types.ObjectId(),
-  first_name: "Bouncing Challenge",
-  last_name: faker.name.findName(),
-  email: faker.internet.email().toLowerCase(),
-  job_title: [`${faker.name.jobTitle()}`],
-  department: faker.commerce.department(),
+  name: "Bouncing Challenge",
+  creator: faker.lorem.words(3).substring(0, 30),
+  tags: shuffle(challengeTags),
+  description: faker.random.words(),
+  location: faker.address.city(),
+  timestamp: faker.date.recent(),
+  start_date: faker.date.soon(),
+  end_date: faker.date.future(),
+  participants: [
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+  ],
 };
 
 const challengeThree = {
   _id: mongoose.Types.ObjectId(),
-  first_name: "CatWalking Challenge",
-  last_name: faker.name.findName(),
-  email: faker.internet.email().toLowerCase(),
-  job_title: [`${faker.name.jobTitle()}`],
-  department: faker.commerce.department(),
+  name: "CatWalking Challenge",
+  creator: faker.lorem.words(3).substring(0, 30),
+  tags: shuffle(challengeTags),
+  description: faker.random.words(),
+  location: faker.address.city(),
+  timestamp: faker.date.recent(),
+  start_date: faker.date.soon(),
+  end_date: faker.date.future(),
+  participants: [
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+    `${faker.name.findName()}`,
+  ],
 };
 
 const insertChallenges = async (challenges) => {
