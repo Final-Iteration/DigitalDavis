73828c237b4341b0a3e0b1b562e3de735d0cfc13
2320sharon <sharonfitzpatrick@csus.edu>
2021-10-17 22:46:57 -0700

    Added fixture & deletes all documents collection

---
+++ app/mongo-api/tests/fixtures/user.fixture.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const faker = require("faker");
+const { User } = require("../../models");
+
+const userOne = {
+  _id: mongoose.Types.ObjectId(),
+  first_name: faker.name.findName(),
+  last_name: faker.name.findName(),
+  email: faker.internet.email().toLowerCase(),
+  job_title: [`${faker.name.jobTitle()}`],
+  department: faker.commerce.department(),
+};
+
+const userTwo = {
+  _id: mongoose.Types.ObjectId(),
+  first_name: faker.name.findName(),
+  last_name: faker.name.findName(),
+  email: faker.internet.email().toLowerCase(),
+  job_title: [`${faker.name.jobTitle()}`],
+  department: faker.commerce.department(),
+};
+
+const insertUsers = async (users) => {
+  await User.insertMany(users.map((user) => ({ ...user })));
+};
+
+module.exports = {
+  userOne,
+  userTwo,
+  insertUsers,
+};
