2e3149fb586dc5dba3db8c8e3cbea575673cf33d
gustavboyd <boydgustav@gmail.com>
2021-10-20 16:24:29 -0700

    created the challenge date screen

---
+++ app/frontend/src/screens/challengesScreen/CreateChallengeDate.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const CreateChallengeDate = () => {
+    return (
+        <View>
+            <Text>
+                Challenge Date Picker Page
+            </Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({
+    text:{
+        marginTop: height / 6,
+    }
+});
+
+export default CreateChallengeDate;
