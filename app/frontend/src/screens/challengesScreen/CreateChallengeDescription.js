10da6fbd99c6d17e60d8bd91eb3fb688b2126416
gustavboyd <boydgustav@gmail.com>
2021-10-20 16:24:20 -0700

    created the challenge description screen

---
+++ app/frontend/src/screens/challengesScreen/CreateChallengeDescription.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const CreateChallengeDescription = () => {
+    return (
+        <View>
+            <Text>
+                Challenge Description Page
+            </Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({
+    text:{
+        marginTop: height / 6,
+    }
+});
+
+export default CreateChallengeDescription;
