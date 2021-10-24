b919983d7a4daeec9cefc6bd81be670a4f940891
Dennis Worley Jr <dennisw95.dw@gmail.com>
2021-10-24 10:29:34 -0700

    [#530] Updated url for today's development session

--- app/frontend/src/axios/index.js
+++ app/frontend/src/axios/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: "http://2bf0-2601-204-e780-d390-b00f-3872-a6cf-3210.ngrok.io/api"
+  baseURL: "http://886d-2601-204-e780-d390-1075-6a31-74d4-8c1e.ngrok.io/api"
 })
