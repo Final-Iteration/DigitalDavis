33b79ca0bd0a6bf247ec6ffa61c1a6af665d3f52
gustavboyd <boydgustav@gmail.com>
2021-10-25 02:29:56 -0700

    flushed out create challenge description page

--- app/frontend/src/screens/challengesScreen/CreateChallengeDescription.js
+++ app/frontend/src/screens/challengesScreen/CreateChallengeDescription.js
@@ -1,21 +1,120 @@
-import React from "react";
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import React, {useState} from "react";
+import {View, Text, StyleSheet, Dimensions, TextInput} from 'react-native';
+import { ProgressBar, Colors, } from 'react-native-paper';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const { width, height } = Dimensions.get('window');
 
 const CreateChallengeDescription = () => {
+    
+    const [nameLength, setInputLength] = useState(30);
+    const [descriptionLength, setDescriptionLength] = useState(250);
+    const [challengeName, setChallengeName] = useState('');
+    const [challengeDescription, setChallengeDescription] = useState('');
+
+    const textInputCount = (prop, text) => {
+        if (prop == 'name'){
+            setInputLength(30 - text.length)
+        }else{
+            setDescriptionLength(250 - text.length)
+        }
+        
+    };
+
     return (
-        <View>
-            <Text>
-                Challenge Description Page
-            </Text>
+        <View style = {styles.container}>
+            <ProgressBar progress={0.75} color={Colors.blue600} />
+            <KeyboardAwareScrollView
+            enableOnAndroid={true}
+            showsVerticalScrollIndicator={false}
+            keyboardShouldPersistTaps={"always"}
+            extraScrollHeight={20}
+            >
+                <Text style = {styles.headerText}>
+                    Give your challenge a name and description. 
+                </Text>
+                <Text style = {styles.secondHeaderText}>
+                    Don't worry about it too much you can change it later. 
+                </Text>
+                <Text style = {styles.nameAndDescriptionText}>
+                    Name
+                </Text>
+                <TextInput 
+                    maxLength={30}
+                    style={styles.nameBox}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={challengeName}
+                    onChangeText={(text) => {setChallengeName(text); textInputCount('name', text);}}
+                />
+                <Text style = {styles.inputLengths}>
+                    {nameLength}
+                </Text>
+                <Text style = {styles.nameAndDescriptionText}>
+                    Description
+                </Text>
+                <TextInput 
+                    maxLength={250}
+                    multiline={true}
+                    style={styles.descriptionBox}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={challengeDescription}
+                    onChangeText={(text) => {setChallengeDescription(text); textInputCount('',text);}}
+                />
+                <Text style = {styles.inputLengths}>
+                    {descriptionLength}
+                </Text>
+            </KeyboardAwareScrollView>
         </View>
     );
 };
 
-const style = StyleSheet.create({
-    text:{
-        marginTop: height / 6,
+const styles = StyleSheet.create({
+    headerText:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginHorizontal: width / 25,
+        marginTop: height / 50
+    },
+    secondHeaderText: {
+        marginHorizontal: width / 25,
+        fontWeight: '300',
+        marginTop: height / 20
+    },
+    nameAndDescriptionText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginHorizontal: width / 25,
+        marginTop: height / 20
+    },
+    container: {
+        flex: 1
+    },
+    inputLengths: {
+        alignSelf: 'flex-end',
+        marginRight: width / 18,
+        top: height / 80
+    },
+    nameBox:{
+        borderRadius: 10,
+        width: width / 1.1,
+        height: height / 25,
+        marginHorizontal: width / 25,
+        borderWidth: width / 300,
+        padding: width / 50,
+        borderColor: '#D3D3D3',
+        marginTop: height / 50
+    },
+    descriptionBox:{
+        borderRadius: 10,
+        width: width / 1.1,
+        height: height / 6,
+        marginHorizontal: width / 25,
+        borderWidth: width / 300,
+        padding: width / 50,
+        borderColor: '#D3D3D3',
+        marginTop: height / 50,
     }
 });
 
