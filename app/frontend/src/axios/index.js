22d2d874f00f013ef018c943f0907aa5f49b2676
Dennis Worley Jr <dennisw95.dw@gmail.com>
2021-10-31 14:16:16 -0700

    [#706] Minor fixes to Axios calls using the correct State variable

--- app/frontend/src/axios/index.js
+++ app/frontend/src/axios/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: " http://4f2f-2601-204-e780-d390-136-5fc7-e2e4-2172.ngrok.io/api"
+  baseURL: "http://d971-2601-204-e780-d390-8903-1938-5db5-3af8.ngrok.io/api"
 })
