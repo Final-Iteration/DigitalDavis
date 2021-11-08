19138c45d91c7c600426cd5acb35f02fca89cffb
gustavboyd <boydgustav@gmail.com>
2021-11-08 03:06:07 +0000

    added tokenTypes to enable refresh tokens and the accompanying services....

--- app/mongo-api/routes/auth.route.js
+++ app/mongo-api/routes/auth.route.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const router = express.Router();
 const validate = require('../middleware/validate');
-const authController = require('../controllers/auth.controller');
 const authValidation = require('../validate/auth.validation');
-const routeDebugger = require('debug')('app:routes');
-const passport = require('passport');
+const authController = require('../controllers/auth.controller');
+const auth = require('../middleware/auth');
 
-router
-  .route('/signup')
-  .post(validate(authValidation.signup), authController.signup);
+const router = express.Router();
 
-router
-  .route('/login')
-  .post(authController.login);
-
-router.get('/test', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-   res.status(200).json({ success: true, msg: 'You are authorized', headers: req.headers});
-});
+router.post('/register', validate(authValidation.register), authController.register);
+router.post('/login', validate(authValidation.login), authController.login);
+router.post('/logout', validate(authValidation.logout), authController.logout);
+router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
+router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
+router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
+router.post('/send-verification-email', auth(), authController.sendVerificationEmail);
+router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail);
+router.get('/test', auth(), (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'You are authorized', headers: req.headers});
+ });
 
 module.exports = router;
