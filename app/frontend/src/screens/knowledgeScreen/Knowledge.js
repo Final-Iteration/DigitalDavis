6dc457782ae4e2658206a642295d331c2db5c204
Manhsy <Manhsy@csus.edu>
2021-11-08 13:28:58 -0800

    added default gif for blank screens

--- app/frontend/src/screens/knowledgeScreen/Knowledge.js
+++ app/frontend/src/screens/knowledgeScreen/Knowledge.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const Knowledge = (props) => {
   return (
-    <View>
-      <Text style={{ alignSelf: "center" }}>This is the Knowledge screen</Text>
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={styles.gif}
+          source={{
+            uri: "http://mauelementaryschool.weebly.com/uploads/8/2/5/7/8257419/141858_orig.gif",
+          }}
+        />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "30%",
+    marginTop: "50%",
+  },
+  gif: {
+    height: "100%",
+    width: "100%",
+  },
+});
 
 export default Knowledge;
