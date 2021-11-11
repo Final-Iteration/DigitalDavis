96395d4c6c0ce013fb232cac6ce3b18e36276744
Manhsy <Manhsy@csus.edu>
2021-11-10 19:20:20 -0800

    remove unused files

--- app/frontend/src/navigators/ChallengesNavigator.js
+++ app/frontend/src/navigators/ChallengesNavigator.js
@@ -5,68 +5,55 @@ import CurrentChallenges from "../screens/challengesScreen/CurrentChallenge";
 import ChallengeInfo from "../screens/challengesScreen/ChallengeInfo";
 import CreateChallengeScreen from "../screens/challengesScreen/CreateChallengeScreen";
 import CreateChallengeScreenTags from "../screens/challengesScreen/CreateChallengeScreenTags";
-import CreateChallengeDate from "../screens/challengesScreen/CreateChallengeDate";
-import CreateChallengeDescription from "../screens/challengesScreen/CreateChallengeDescription";
 
 import Header from "../sharedComponent/Header";
 
-const ChallengeStack = createStackNavigator({
-  Challenge: {
-    screen: CurrentChallenges,
-    navigationOptions: {
-      header: ({ scene, previous, navigation }) => (
-        <Header title="Challenges" navigation={navigation} challenge={true} />
-      ),
+const ChallengeStack = createStackNavigator(
+  {
+    Challenge: {
+      screen: CurrentChallenges,
+      navigationOptions: {
+        header: ({ scene, previous, navigation }) => (
+          <Header title="Challenges" navigation={navigation} challenge={true} />
+        ),
+      },
     },
-  },
 
-  ChallengeInformation: {
-    screen: ChallengeInfo,
-    navigationOptions: {
-      header: ({ scene, previous, navigation }) => (
-        <Header challengeInfo={true} navigation={navigation} challenge={true} />
-      ),
+    ChallengeInformation: {
+      screen: ChallengeInfo,
+      navigationOptions: {
+        header: ({ scene, previous, navigation }) => (
+          <Header
+            challengeInfo={true}
+            navigation={navigation}
+            challenge={true}
+          />
+        ),
+      },
+    },
+    CreateChallenge: {
+      screen: CreateChallengeScreen,
+      navigationOptions: {
+        header: ({ scene, previous, navigation }) => (
+          <Header
+            createChallenge={true}
+            title="Create Challenge"
+            navigation={navigation}
+          />
+        ),
+      },
+    },
+    CreateChallengeTags: {
+      screen: CreateChallengeScreenTags,
+      navigationOptions: {
+        // header: ({ scene, previous, navigation }) => (
+        //   <Header CreateChallengeTags={true} navigation={navigation} />
+        // ),
+        headerShown: false,
+      },
     },
   },
-  CreateChallenge: {
-    screen: CreateChallengeScreen,
-    navigationOptions: {
-      header: ({ scene, previous, navigation }) => (
-        <Header
-          createChallenge={true}
-          title="Create Challenge"
-          navigation={navigation}
-        />
-      ),
-    },
-  },
-  CreateChallengeTags: {
-    screen: CreateChallengeScreenTags,
-    navigationOptions: {
-      // header: ({ scene, previous, navigation }) => (
-      //   <Header CreateChallengeTags={true} navigation={navigation} />
-      // ),
-      headerShown: false,
-    },
-  },
-  CreateChallengeDate: {
-    screen: CreateChallengeDate,
-    navigationOptions: {
-      header: ({ scene, previous, navigation }) => (
-        <Header CreateChallengeDate={true} navigation={navigation} />
-      ),
-    },
-  },
-  CreateChallengeDescription: {
-    screen: CreateChallengeDescription,
-    navigationOptions: {
-      header: ({ scene, previous, navigation }) => (
-        <Header CreateChallengeDescription={true} navigation={navigation} />
-      ),
-    },
-  },
-},
-{headerMode: "screen"}
+  { headerMode: "screen" }
 );
 
 export default ChallengeStack;
