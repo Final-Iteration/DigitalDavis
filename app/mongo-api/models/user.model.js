c104af55c625cab3f32b53a0f8fa16b032cd5704
Daniel Olsen <dolsen@csus.edu>
2021-11-21 19:47:11 -0800

    added check to make sure password has changed before rehashing in pre save function

--- app/mongo-api/models/user.model.js
+++ app/mongo-api/models/user.model.js
@@ -97,6 +97,7 @@ userSchema.methods.passwordMatch = async function (password) {
 };
 
 userSchema.pre("save", async function (next) {
+  if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
