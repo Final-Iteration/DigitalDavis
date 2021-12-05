002459171858604aafb794c0bd5d428c68977518
Daniel Olsen <dolsen@csus.edu>
2021-12-05 11:23:03 -0800

    changed token to be in body

--- app/mongo-api/controllers/auth.controller.js
+++ app/mongo-api/controllers/auth.controller.js
@@ -34,7 +34,7 @@ const forgotPassword = catchAsync(async (req, res) => {
 });
 
 const resetPassword = catchAsync(async (req, res) => {
-  await authService.resetPassword(req.query.token, req.body.password);
+  await authService.resetPassword(req.body.token, req.body.password);
   res.status(httpStatus.NO_CONTENT).send();
 });
 
