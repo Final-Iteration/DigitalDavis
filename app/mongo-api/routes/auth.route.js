aefa46d34ec74790fb293fd96273e28327321ae7
Daniel Olsen <dolsen@csus.edu>
2021-12-05 11:23:23 -0800

    took away validation for now. will add back when validation functions are completed

--- app/mongo-api/routes/auth.route.js
+++ app/mongo-api/routes/auth.route.js
@@ -10,8 +10,8 @@ router.post('/register', validate(authValidation.register), authController.regis
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
-router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
-router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 router.post('/send-verification-email', auth(), authController.sendVerificationEmail);
 router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail);
 router.get('/test', auth(), (req, res, next) => {
