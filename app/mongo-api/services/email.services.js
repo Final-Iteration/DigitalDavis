b2a0609461a5a9b14c76985b2a7dcb7940a1032b
Daniel Olsen <dolsen@csus.edu>
2021-12-05 11:21:43 -0800

    LInk will need to be a config variable. However it works with localhost now

--- app/mongo-api/services/email.services.js
+++ app/mongo-api/services/email.services.js
@@ -13,12 +13,12 @@ const options = {
     from: "finaliteration@outlook.com",
     to: "",
     subject: "Nirvana Password Reset",
-    text: "Please copy and paste this link into your web browser to reset your password: "
+    text: "Please use this link to reset your password: "
 };
 
 const sendResetPasswordEmail = async (to, token) => {
     options.to = to;
-    options.text = "Please copy and paste this link into your web browser to reset your password: http://127.0.0.1:5500/website/hello.html?" + token;
+    options.text = "Please use this link to reset your password: http://127.0.0.1:3000/?" + token;
     transport.sendMail(options, function (err, info) {
         if(err){
             console.log(err);
